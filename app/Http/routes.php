44654897772c540fbdcf2dc721342efaea27eba8
uzaif313 <uzaif313@gmail.com>
2016-08-09 07:34:45 +0530

    Added Multiple Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,11 @@ $route_function = function(){
 	return "<h1>India is better home than other country</h1>";
 };
 Route::get('india/home',$route_function);
+
+Route::get('canada/second_home',function(){
+	return "<h2>Like to live  there with family!!</h2>";
+});
+
+Route::get('europ/third_home',function(){
+	return "<h2>Like to live and work here !!</h2>";
+});
